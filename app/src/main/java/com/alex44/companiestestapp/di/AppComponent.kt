f81a55ed310312db22504d53666face253e0ba5c
Alexandr44 <alexandrgluhov@mail.ru>
2020-01-29 04:29:45 +0500

    Методы для DI

--- app/src/main/java/com/alex44/companiestestapp/di/AppComponent.kt
+++ app/src/main/java/com/alex44/companiestestapp/di/AppComponent.kt
@@ -4,10 +4,12 @@ import com.alex44.companiestestapp.di.modules.AppModule
 import com.alex44.companiestestapp.di.modules.CiceroneModule
 import com.alex44.companiestestapp.di.modules.ImageModule
 import com.alex44.companiestestapp.di.modules.RepoModule
+import com.alex44.companiestestapp.presenters.DetailPresenter
 import com.alex44.companiestestapp.presenters.HomePresenter
 import com.alex44.companiestestapp.presenters.MainPresenter
 import com.alex44.companiestestapp.ui.activities.MainActivity
 import com.alex44.companiestestapp.ui.adapters.HomeRVAdapter
+import com.alex44.companiestestapp.ui.fragments.DetailFragment
 import dagger.Component
 import javax.inject.Singleton
 
@@ -19,5 +21,7 @@ interface AppComponent {
     fun inject(mainActivity: MainPresenter)
     fun inject(presenter: HomePresenter)
     fun inject(homeRVAdapter: HomeRVAdapter)
+    fun inject(detailFragment: DetailFragment)
+    fun inject(presenter: DetailPresenter)
 
 }
