4f7f1485384310d7527d66c46afca29d68aac525
Alexandr44 <alexandrgluhov@mail.ru>
2020-01-28 16:12:34 +0500

    Строки апи (base url)

---
+++ app/src/main/java/com/alex44/companiestestapp/common/model/api/ApiStrings.kt
@@ -0,0 +1,11 @@
+package com.alex44.companiestestapp.common.model.api
+
+class ApiStrings {
+
+    companion object {
+
+        val RETROFIT_BASE_URL = "http://megakohz.bget.ru/test_task/"
+
+    }
+
+}
