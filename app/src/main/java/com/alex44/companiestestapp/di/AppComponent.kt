6fc95b2edffe8e7da01b2ab234284ac907955a41
Alexandr44 <alexandrgluhov@mail.ru>
2020-01-28 17:11:51 +0500

    Добавил MVP к MainActivity, поменял layout, добавил методы для DI в AppComponent

--- app/src/main/java/com/alex44/companiestestapp/di/AppComponent.kt
+++ app/src/main/java/com/alex44/companiestestapp/di/AppComponent.kt
@@ -3,11 +3,16 @@ package com.alex44.companiestestapp.di
 import com.alex44.companiestestapp.di.modules.AppModule
 import com.alex44.companiestestapp.di.modules.CiceroneModule
 import com.alex44.companiestestapp.di.modules.ImageModule
+import com.alex44.companiestestapp.presenters.MainPresenter
+import com.alex44.companiestestapp.ui.activities.MainActivity
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [AppModule::class, ImageModule::class, CiceroneModule::class])
 interface AppComponent {
+
+    fun inject(mainActivity: MainActivity)
+    fun inject(mainActivity: MainPresenter)
 
 }
---
+++ app/src/main/java/com/alex44/companiestestapp/presenters/MainPresenter.kt
@@ -0,0 +1,24 @@
+package com.alex44.companiestestapp.presenters
+
+import com.alex44.companiestestapp.views.MainView
+import com.arellomobile.mvp.InjectViewState
+import com.arellomobile.mvp.MvpPresenter
+import ru.terrakok.cicerone.Router
+import javax.inject.Inject
+
+@InjectViewState
+class MainPresenter : MvpPresenter<MainView>() {
+
+    @Inject
+    lateinit var router: Router
+
+    override fun onFirstViewAttach() {
+        super.onFirstViewAttach()
+        goToHomeScreen()
+    }
+
+    private fun goToHomeScreen() {
+
+    }
+
+}
