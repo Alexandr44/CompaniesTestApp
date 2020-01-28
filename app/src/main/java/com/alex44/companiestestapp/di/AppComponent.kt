1392012e083eb6a624d174f5e3cfc7b625512000
Alexandr44 <alexandrgluhov@mail.ru>
2020-01-29 02:58:17 +0500

    Методы DI

--- app/src/main/java/com/alex44/companiestestapp/di/AppComponent.kt
+++ app/src/main/java/com/alex44/companiestestapp/di/AppComponent.kt
@@ -3,16 +3,21 @@ package com.alex44.companiestestapp.di
 import com.alex44.companiestestapp.di.modules.AppModule
 import com.alex44.companiestestapp.di.modules.CiceroneModule
 import com.alex44.companiestestapp.di.modules.ImageModule
+import com.alex44.companiestestapp.di.modules.RepoModule
+import com.alex44.companiestestapp.presenters.HomePresenter
 import com.alex44.companiestestapp.presenters.MainPresenter
 import com.alex44.companiestestapp.ui.activities.MainActivity
+import com.alex44.companiestestapp.ui.adapters.HomeRVAdapter
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AppModule::class, ImageModule::class, CiceroneModule::class])
+@Component(modules = [AppModule::class, ImageModule::class, CiceroneModule::class, RepoModule::class])
 interface AppComponent {
 
     fun inject(mainActivity: MainActivity)
     fun inject(mainActivity: MainPresenter)
+    fun inject(presenter: HomePresenter)
+    fun inject(homeRVAdapter: HomeRVAdapter)
 
 }
