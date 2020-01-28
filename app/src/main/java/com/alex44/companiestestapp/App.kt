0d103e2bf9f125a0c65f6b4f39871fb46f5c9bfc
Alexandr44 <alexandrgluhov@mail.ru>
2020-01-28 16:13:42 +0500

    Доработал Класс App

--- app/src/main/java/com/alex44/companiestestapp/App.kt
+++ app/src/main/java/com/alex44/companiestestapp/App.kt
@@ -1,4 +1,19 @@
 package com.alex44.companiestestapp
 
-class App {
+import android.app.Application
+import timber.log.Timber
+
+
+class App : Application() {
+
+    companion object{
+        lateinit var instance : App
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        Timber.plant(Timber.DebugTree())
+    }
+
 }
