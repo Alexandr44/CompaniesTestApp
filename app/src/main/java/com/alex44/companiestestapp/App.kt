64098e64718d8e45deb741a0f864d663c4c4ca78
Alexandr44 <alexandrgluhov@mail.ru>
2020-01-28 16:04:46 +0500

    Поправил манифест

---
+++ app/src/main/java/com/alex44/companiestestapp/App.kt
@@ -0,0 +1,4 @@
+package com.alex44.companiestestapp
+
+class App {
+}
--- app/src/main/java/com/alex44/companiestestapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/alex44/companiestestapp/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.alex44.companiestestapp
+package com.alex44.companiestestapp.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alex44.companiestestapp.R
 
 class MainActivity : AppCompatActivity() {
 
